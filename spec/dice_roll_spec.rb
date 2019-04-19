623d224aa2c0b2b63e3a52fd575667d5bad1e05b
Shemmyla Green <shemmyla@hotmail.com>
2019-04-19 05:22:32 +0000

    Automatically backed up by Learn

--- spec/dice_roll_spec.rb
+++ spec/dice_roll_spec.rb
@@ -4,12 +4,13 @@ describe "#roll" do
 
   it "should return an Integer" do
     expect(roll).to be_a(Integer)
-    rand(8)
+    rand()
 
   end
 
   it "should return a number greater than 0" do
     expect(roll).to be > 0
+    rand(8)
     
   end
 
