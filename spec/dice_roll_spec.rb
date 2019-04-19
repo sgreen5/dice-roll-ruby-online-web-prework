b4fa5918c39ed8da5c20b596e04fb5cf2aba4a79
Shemmyla Green <shemmyla@hotmail.com>
2019-04-19 05:30:52 +0000

    Done.

--- spec/dice_roll_spec.rb
+++ spec/dice_roll_spec.rb
@@ -4,23 +4,30 @@ describe "#roll" do
 
   it "should return an Integer" do
     expect(roll).to be_a(Integer)
+    rand()
+
   end
 
   it "should return a number greater than 0" do
     expect(roll).to be > 0
+    rand(8)
+    
   end
 
   it "should return a number less than 7" do
     expect(roll).to be < 7
+    rand(1...6)
   end
 
   it "should return a random value" do
     rolls = []
     100.times do
       rolls << roll
+      100.times {rand (1...75)
     end
     expect(rolls).to include(1, 2, 3, 4, 5, 6)
     expect(rolls).not_to include(0, 7)
+    rand(1...6)
   end
 
 end
